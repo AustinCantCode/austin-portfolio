22f82aecbbf0f49267d95f25bfd58f854d0e7d50
Austin Sia <austin@ambercreative.sg>
2025-08-31 16:31:50 +0800

    feat: added SEO improvements to main index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import NameBanner from "@components/complex-ui/name-banner";
 import { useEffect, useState } from "react";
+import { NextSeo } from "next-seo";
 
 export default function Homepage() {
   const [fadeIn, setFadeIn] = useState("");
@@ -13,12 +14,50 @@ export default function Homepage() {
   }, []);
 
   return (
-    <main className="px-4 md:px-0 relative overflow-hidden min-h-screen flex justify-center bg-black">
-      {/* Background Layers */}
-      <div className={"absolute inset-0 z-0" + fadeIn} />
-      <div className="absolute inset-0 bg-twinkling z-10" />
-      {/* Name Card */}
-      <NameBanner />
-    </main>
+    <>
+      <NextSeo
+        title="Austin Sia"
+        description="The personal portfolio of Austin Sia. Explore projects in web development, UI/UX, and software design."
+        canonical="https://austinsia.com/"
+        openGraph={{
+          url: "https://austinsia.com/",
+          title: "Austin Sia",
+          description:
+            "The personal portfolio of Austin Sia. Explore projects in web development, UI/UX, and software design.",
+          images: [
+            {
+              url: "https://austinsia.com/AS-Circle-Logo.png",
+              width: 500,
+              height: 500,
+              alt: "AS Logo",
+            },
+          ],
+          site_name: "AustinSia",
+        }}
+      />
+      <main className="px-4 md:px-0 relative overflow-hidden min-h-screen flex justify-center bg-black">
+        {/* Background Layers */}
+        <div className={"absolute inset-0 z-0" + fadeIn} />
+        <div className="absolute inset-0 bg-twinkling z-10" />
+        {/* Name Card */}
+        <NameBanner />
+      </main>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            name: "Austin Sia",
+            url: "https://austinsia.com",
+            sameAs: [
+              "https://www.linkedin.com/in/austin-sia",
+              "https://github.com/austincantcode",
+            ],
+            jobTitle: "Web Developer",
+          }),
+        }}
+      />
+    </>
   );
 }
