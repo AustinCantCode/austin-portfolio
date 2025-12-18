5f4d542e996a0f7f84f8470fc100a8206bca60a5
Austin Sia <austin.sia1515@gmail.com>
2025-12-18 19:21:52 +0800

    fix: updated carousel logo

--- src/data/achievement-logos.tsx
+++ src/data/achievement-logos.tsx
@@ -1,17 +1,66 @@
 // LOGOS
+import aws from "../../public/achievement/logo/aws.png";
+import github from "../../public/achievement/logo/github.png";
+import batey from "../../public/achievement/logo/batey.png";
+import linkedin from "../../public/achievement/logo/linkedin.png";
+import sp from "../../public/achievement/logo/sp.png";
+import moe from "../../public/achievement/logo/moe.png";
+import pa from "../../public/achievement/logo/pa.png";
+import stem from "../../public/achievement/logo/stem.png";
+import cvss from "../../public/achievement/logo/cvss.png";
+import scs from "../../public/achievement/logo/scs.jpg";
+import sololearn from "../../public/achievement/logo/sololearn.png";
+import mozilla from "../../public/achievement/logo/mozilla.png";
+
 const achievementLogos = [
-  { id: 1, image: "/achievements/logo/aws.png" },
-  { id: 2, image: "/achievements/logo/github.png" },
-  { id: 3, image: "/achievements/logo/batey.png" },
-  { id: 4, image: "/achievements/logo/linkedin.png" },
-  { id: 5, image: "/achievements/logo/sp.png" },
-  { id: 6, image: "/achievements/logo/moe.png" },
-  { id: 7, image: "/achievements/logo/pa.png" },
-  { id: 8, image: "/achievements/logo/stem.png" },
-  { id: 9, image: "/achievements/logo/cvss.png" },
-  { id: 10, image: "/achievements/logo/scs.jpg" },
-  { id: 11, image: "/achievements/logo/sololearn.png" },
-  { id: 12, image: "/achievements/logo/mozilla.png" },
+  {
+    id: 1,
+    image: aws,
+  },
+  {
+    id: 2,
+    image: github,
+  },
+  {
+    id: 3,
+    image: batey,
+  },
+  {
+    id: 4,
+    image: linkedin,
+  },
+  {
+    id: 5,
+    image: sp,
+  },
+  {
+    id: 6,
+    image: moe,
+  },
+  {
+    id: 7,
+    image: pa,
+  },
+  {
+    id: 8,
+    image: stem,
+  },
+  {
+    id: 9,
+    image: cvss,
+  },
+  {
+    id: 10,
+    image: scs,
+  },
+  {
+    id: 11,
+    image: sololearn,
+  },
+  {
+    id: 12,
+    image: mozilla,
+  },
 ];
 
 export default achievementLogos;
