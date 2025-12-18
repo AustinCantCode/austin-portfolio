bc729dbf2a917f53796b87d8fd66c90bcc70c703
Austin Sia <austin.sia1515@gmail.com>
2025-12-18 17:01:49 +0800

    fix: build error

--- src/data/achievement-logos.tsx
+++ src/data/achievement-logos.tsx
@@ -1,66 +1,83 @@
-// LOGOS
-import aws from "@achievements/logos/aws.png";
-import github from "@achievements/logos/github.png";
-import batey from "@achievements/logos/batey.png";
-import linkedin from "@achievements/logos/linkedin.png";
-import sp from "@achievements/logos/sp.png";
-import moe from "@achievements/logos/moe.png";
-import pa from "@achievements/logos/pa.png";
-import stem from "@achievements/logos/stem.png";
-import cvss from "@achievements/logos/cvss.png";
-import scs from "@achievements/logos/scs.jpg";
-import sololearn from "@achievements/logos/sololearn.png";
-import mozilla from "@achievements/logos/mozilla.png";
+// // LOGOS
+// import aws from "@achievements/logos/aws.png";
+// import github from "@achievements/logos/github.png";
+// import batey from "@achievements/logos/batey.png";
+// import linkedin from "@achievements/logos/linkedin.png";
+// import sp from "@achievements/logos/sp.png";
+// import moe from "@achievements/logos/moe.png";
+// import pa from "@achievements/logos/pa.png";
+// import stem from "@achievements/logos/stem.png";
+// import cvss from "@achievements/logos/cvss.png";
+// import scs from "@achievements/logos/scs.jpg";
+// import sololearn from "@achievements/logos/sololearn.png";
+// import mozilla from "@achievements/logos/mozilla.png";
+
+// const achievementLogos = [
+//   {
+//     id: 1,
+//     image: aws,
+//   },
+//   {
+//     id: 2,
+//     image: github,
+//   },
+//   {
+//     id: 3,
+//     image: batey,
+//   },
+//   {
+//     id: 4,
+//     image: linkedin,
+//   },
+//   {
+//     id: 5,
+//     image: sp,
+//   },
+//   {
+//     id: 6,
+//     image: moe,
+//   },
+//   {
+//     id: 7,
+//     image: pa,
+//   },
+//   {
+//     id: 8,
+//     image: stem,
+//   },
+//   {
+//     id: 9,
+//     image: cvss,
+//   },
+//   {
+//     id: 10,
+//     image: scs,
+//   },
+//   {
+//     id: 11,
+//     image: sololearn,
+//   },
+//   {
+//     id: 12,
+//     image: mozilla,
+//   },
+// ];
+
+// export default achievementLogos;
 
 const achievementLogos = [
-  {
-    id: 1,
-    image: aws,
-  },
-  {
-    id: 2,
-    image: github,
-  },
-  {
-    id: 3,
-    image: batey,
-  },
-  {
-    id: 4,
-    image: linkedin,
-  },
-  {
-    id: 5,
-    image: sp,
-  },
-  {
-    id: 6,
-    image: moe,
-  },
-  {
-    id: 7,
-    image: pa,
-  },
-  {
-    id: 8,
-    image: stem,
-  },
-  {
-    id: 9,
-    image: cvss,
-  },
-  {
-    id: 10,
-    image: scs,
-  },
-  {
-    id: 11,
-    image: sololearn,
-  },
-  {
-    id: 12,
-    image: mozilla,
-  },
+  { id: 1, image: "/achievements/logos/aws.png" },
+  { id: 2, image: "/achievements/logos/github.png" },
+  { id: 3, image: "/achievements/logos/batey.png" },
+  { id: 4, image: "/achievements/logos/linkedin.png" },
+  { id: 5, image: "/achievements/logos/sp.png" },
+  { id: 6, image: "/achievements/logos/moe.png" },
+  { id: 7, image: "/achievements/logos/pa.png" },
+  { id: 8, image: "/achievements/logos/stem.png" },
+  { id: 9, image: "/achievements/logos/cvss.png" },
+  { id: 10, image: "/achievements/logos/scs.jpg" },
+  { id: 11, image: "/achievements/logos/sololearn.png" },
+  { id: 12, image: "/achievements/logos/mozilla.png" },
 ];
 
 export default achievementLogos;
