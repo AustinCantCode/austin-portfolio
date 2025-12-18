c434078b94bbfeffbb6e2bb3a7917e3b9662f240
Austin Sia <austin.sia1515@gmail.com>
2025-12-18 18:59:35 +0800

    feat: updated file name

--- src/data/achievement-logos.tsx
+++ src/data/achievement-logos.tsx
@@ -1,17 +1,17 @@
 // LOGOS
 const achievementLogos = [
-  { id: 1, image: "/achievements/logos/aws.png" },
-  { id: 2, image: "/achievements/logos/github.png" },
-  { id: 3, image: "/achievements/logos/batey.png" },
-  { id: 4, image: "/achievements/logos/linkedin.png" },
-  { id: 5, image: "/achievements/logos/sp.png" },
-  { id: 6, image: "/achievements/logos/moe.png" },
-  { id: 7, image: "/achievements/logos/pa.png" },
-  { id: 8, image: "/achievements/logos/stem.png" },
-  { id: 9, image: "/achievements/logos/cvss.png" },
-  { id: 10, image: "/achievements/logos/scs.jpg" },
-  { id: 11, image: "/achievements/logos/sololearn.png" },
-  { id: 12, image: "/achievements/logos/mozilla.png" },
+  { id: 1, image: "/achievements/logo/aws.png" },
+  { id: 2, image: "/achievements/logo/github.png" },
+  { id: 3, image: "/achievements/logo/batey.png" },
+  { id: 4, image: "/achievements/logo/linkedin.png" },
+  { id: 5, image: "/achievements/logo/sp.png" },
+  { id: 6, image: "/achievements/logo/moe.png" },
+  { id: 7, image: "/achievements/logo/pa.png" },
+  { id: 8, image: "/achievements/logo/stem.png" },
+  { id: 9, image: "/achievements/logo/cvss.png" },
+  { id: 10, image: "/achievements/logo/scs.jpg" },
+  { id: 11, image: "/achievements/logo/sololearn.png" },
+  { id: 12, image: "/achievements/logo/mozilla.png" },
 ];
 
 export default achievementLogos;
